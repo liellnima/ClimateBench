84fadf9038dbacdc30ffbec2e8a1e51ab9c932c0
Charlie Lange <charlie.lange@posteo.de>
2022-04-07 20:42:45 +0200

    smaller env

--- path_parameters.py
+++ path_parameters.py
@@ -1,6 +1,6 @@
 # CONFIGURE ME
-TRAIN_PATH = "~/Documents/Master/CausalSuperEmulator/Data/train_val/"
-TEST_PATH = "~/Documents/Master/CausalSuperEmulator/Data/test/"
-OUTPUT_PATH = "~/Documents/Master/CausalSuperEmulator/Data/outputs/"
+TRAIN_PATH = "/home/charlie/Documents/MILA/ClimateBench/ClimateBench_Data/train_val/"
+TEST_PATH = "/home/charlie/Documents/MILA/ClimateBench/ClimateBench_Data/test/"
+OUTPUT_PATH = "/home/charlie/Documents/MILA/ClimateBench/ClimateBench_Data/outputs/"
 
 # TODO add main method to set paths via command line
